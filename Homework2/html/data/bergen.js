2962d48740a486569afbc86cc8f2863264416baa
CharlieHaines <chainesy17@gmail.com>
2026-01-22 15:39:49 -0500

    Homework 2
    This is the leaflet homework

---
+++ Homework2/html/data/bergen.js
@@ -0,0 +1,7 @@
+var UiB = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "descrip": "UiB" }, "geometry": { "type": "Point", "coordinates": [ 5.3244, 60.3885 ] } }
+]
+}
